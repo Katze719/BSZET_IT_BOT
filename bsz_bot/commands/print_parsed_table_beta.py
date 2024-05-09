c84fb79e37bb718e5025cd95c9603bdffa67ef90
Katze719 <katze719@users.noreply.github.com>
2024-05-09 11:56:48 +0200

    correct parse table function

--- bsz_bot/commands/print_parsed_table_beta.py
+++ bsz_bot/commands/print_parsed_table_beta.py
@@ -2,7 +2,7 @@ import discord
 import os
 from ..helpers import *
 
-@discord.app_commands.command(name="print_parsed_table_beta", description="print the parsed table")
+@discord.app_commands.command(name="print_parsed_table_experimental", description="print the parsed table")
 async def print_parsed_table_beta(ctx : discord.Interaction):
     parsed_dict = parse_table(f'{os.getenv('SETTINGS_VOLUME')}/{GuildSettings(ctx.guild).get("output_name")}.pdf')
     msg = ""
@@ -10,7 +10,7 @@ async def print_parsed_table_beta(ctx : discord.Interaction):
     for class_name, events in parsed_dict.items():
         msg += f"Class: {class_name}\n"
         for event in events:
-            msg += f"{event}\n"
+            msg += f"{event["full_info"]}\n"
             pass
             
     await ctx.response.send_message(embed=simple_embed('Print Parsed Table', msg))
