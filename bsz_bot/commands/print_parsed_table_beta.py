07ea0b3c93283b19e203fc1680f334faa564b200
Katze719 <katze719@users.noreply.github.com>
2024-05-10 21:03:18 +0200

    patch

--- bsz_bot/commands/print_parsed_table_beta.py
+++ bsz_bot/commands/print_parsed_table_beta.py
@@ -4,7 +4,7 @@ from ..helpers import *
 
 @discord.app_commands.command(name="print_parsed_table_experimental", description="print the parsed table")
 async def print_parsed_table_beta(ctx : discord.Interaction):
-    parsed_dict = parse_table(f'{os.getenv('SETTINGS_VOLUME')}/{GuildSettings(ctx.guild).get("output_name")}.pdf')
+    parsed_dict = parse_table(f'{os.getenv("SETTINGS_VOLUME")}/{GuildSettings(ctx.guild).get("output_name")}.pdf')
     msg = ""
     
     for class_name, events in parsed_dict.items():
