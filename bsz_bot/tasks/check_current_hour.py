6df0d18b89054e6f6a00d4c05cc2a91277b237de
Katze719 <katze719@users.noreply.github.com>
2024-05-15 18:49:52 +0200

    hot fix

--- bsz_bot/tasks/check_current_hour.py
+++ bsz_bot/tasks/check_current_hour.py
@@ -6,7 +6,7 @@ from ..helpers import *
 
 scheduler = AsyncIOScheduler()
 
-async def get_news(id, time):
+async def get_news(id):
     logger.info("sending hourly news")
 
     for guild in BSZ_BOT.guilds:
@@ -18,6 +18,7 @@ async def get_news(id, time):
         if s.get("routine") != True:
             continue
 
+        await Plan(guild).download()
         parsed_plan = parse_table(f'{os.getenv("SETTINGS_VOLUME")}/{Plan(guild).get_file_name()}.pdf')
 
         channel = BSZ_BOT.get_channel(int(s.get("routine_channel_id")))
@@ -28,13 +29,11 @@ async def get_news(id, time):
 
         for event in parsed_plan:
             if s.get("class") in event["class"]:
-                if is_today(event["date"]) and event["position"] == id:
+                if is_today(event["date"]) and f"{event["position"]}" == f"{id}":
                     msg += f"Stunde: {event['hours']}\nLehrer: {event['teacher']}\nFach:   {event['subject']}\nRaum:   {event['room']}\nInfo:   {event['info']}\n\n"
 
         if msg != '':
             await channel.send(embed=simple_embed(f'Jetzt!', f"```txt\n{msg}\n```"))
-        else:
-            await channel.send(embed=simple_embed(f'Jetzt!', 'Keine Neuigkeiten'))
 
 times_with_ids = {
     "7:10": 1,
@@ -46,4 +45,4 @@ times_with_ids = {
 # Füge die Jobs für die angegebenen Zeiten hinzu
 for time, id in times_with_ids.items():
     hour, minute = map(int, time.split(':'))
-    scheduler.add_job(get_news, CronTrigger(hour=hour, minute=minute, day_of_week='mon-fri'), args=[id, time])
+    scheduler.add_job(get_news, CronTrigger(hour=hour, minute=minute, day_of_week='mon-fri'), args=[id])
