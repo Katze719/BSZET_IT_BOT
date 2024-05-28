75f9eaeed79e3956f5816ea12d1add7d92f411ac
Katze719 <katze719@users.noreply.github.com>
2024-05-28 21:50:29 +0200

    add

--- bsz_bot/commands/plan.py
+++ bsz_bot/commands/plan.py
@@ -16,7 +16,7 @@ async def plan(ctx : discord.Interaction):
     error_code = await plan.download()
     if error_code != 200:
         s = GuildSettings(ctx.guild)
-        await ctx.response.send_message(embed=simple_embed(f"request: {error_code}", f"""ERROR: the pdf document could not be downloaded with:
+        await ctx.response.send_message(embed=simple_embed(f"Error: {error_code}", f"""ERROR: the pdf document could not be downloaded with:
                                                     `File URL: {s.get('file_url')}`
                                                     `Username: {s.get('username')}`
                                                     `Password: {s.get('password')}`
