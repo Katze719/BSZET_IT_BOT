fceaa3c2bdb02fc15ae33a5a10e877082ecfa875
Katze719 <katze719@users.noreply.github.com>
2024-05-28 21:42:45 +0200

    add more logging

--- bsz_bot/commands/plan.py
+++ bsz_bot/commands/plan.py
@@ -17,9 +17,24 @@ async def plan(ctx : discord.Interaction):
     if error_code != 200:
         s = GuildSettings(ctx.guild)
         await ctx.response.send_message(embed=simple_embed(f"request: {error_code}", f"""ERROR: the pdf document could not be downloaded with:
-                                                           File URL: {s.get('file_url')}
-                                                           Username: {s.get('username')}
-                                                           Password: {s.get('password')}"""))
+                                                    `File URL: {s.get('file_url')}`
+                                                    `Username: {s.get('username')}`
+                                                    `Password: {s.get('password')}`
+                                                    
+                                                    The Bot will now stop sending messages until the error is fixed.
+                                                    
+                                                    Debugging tips:
+                                                    
+                                                    - use `/plan` from now on, since the Bot stoped sending messages
+                                                    - try `/get all` to see all variables
+                                                    - try changing the `file_url` with `/set file_url <new value>`
+                                                    - try changing the `username` with `/set username <new value>`
+                                                    - try changing the `password` with `/set password <new value>`
+                                                    - if you want to change a value back to default, use\n`/set <variable> (use default)` or `/reset`\n
+                                                    If that doesn't work, please open an issue on the GitHub repository.
+                                                    https://github.com/Katze719/BSZET_IT_BOT
+                                                    
+                                                    """))
         return
     
     file = discord.File(f"{plan.get_file_name()}.png")
